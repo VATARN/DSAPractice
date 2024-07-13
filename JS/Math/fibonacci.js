0da69c18a3fcef597ee91d8f0e8e7510087f970d
Arnav Vats <arnav.vats@utexas.edu>
2024-07-13 16:17:30 -0700

    Algo structure and fibonacci

---
+++ JS/Math/fibonacci.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+console.log(fibonacci(5)); // [0, 1, 1, 2, 3]
+console.log(fibonacci(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55] 
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
